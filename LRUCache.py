53f5ad1d876f2e083e5592826a7e9a938905eb55
GSK-123 <155856954+GSK-123@users.noreply.github.com>
2024-07-05 10:20:42 -0700

    Add files via upload
    first commit

---
+++ LRUCache.py
@@ -0,0 +1,57 @@
+class Node:
+    def __init__(self, key, value):
+        self.key = key
+        self.value = value
+        self.prev = None
+        self.next = None
+
+class LRUCache:
+    def __init__(self, capacity):
+        self.capacity = capacity
+        self.cache = {}
+        self.head = Node(0, 0)
+        self.tail = Node(0, 0)
+        self.head.next = self.tail
+        self.tail.prev = self.head
+
+    def _add_node(self, node):
+        node.prev = self.head
+        node.next = self.head.next
+        self.head.next.prev = node
+        self.head.next = node
+
+    def _remove_node(self, node):
+        prev = node.prev
+        next = node.next
+        prev.next = next
+        next.prev = prev
+
+    def _move_to_head(self, node):
+        self._remove_node(node)
+        self._add_node(node)
+
+    def _pop_tail(self):
+        res = self.tail.prev
+        self._remove_node(res)
+        return res
+
+    def get(self, key):
+        node = self.cache.get(key)
+        if not node:
+            return -1
+        self._move_to_head(node)
+        return node.value
+
+    def put(self, key, value):
+        node = self.cache.get(key)
+        if node:
+            node.value = value
+            self._move_to_head(node)
+        else:
+            new_node = Node(key, value)
+            self.cache[key] = new_node
+            self._add_node(new_node)
+            if len(self.cache) > self.capacity:
+                tail = self._pop_tail()
+                del self.cache[tail.key]
+
